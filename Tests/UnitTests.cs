d997e7296abd0631a3e12201dd504810d72d4ddd
Lucas Coelho <lmoreiradesouzacoe2@huskers.unl.edu>
2025-03-28 12:20:47 -0500

    feat/fix error

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ public class CalculatorTests
 	[TestMethod]
 	public void Add_Valid_Patino()
 	{
-		Assert.AreEqual(2, Program.Add("1", "2"));
+		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
