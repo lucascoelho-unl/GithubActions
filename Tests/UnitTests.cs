d6db3ae69794efe7d041744c0ad8254a34b775da
Lucas Coelho <lmoreiradesouzacoe2@huskers.unl.edu>
2025-03-28 12:18:31 -0500

    feat/failed test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ public class CalculatorTests
 	[TestMethod]
 	public void Add_Valid_Patino()
 	{
-		Assert.AreEqual(3, Program.Add("1", "2"));
+		Assert.AreEqual(2, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
@@ -101,12 +101,6 @@ public class CalculatorTests
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
-	}
-
-	[TestMethod]
-	public void Divide_ByZero()
-	{
-		Assert.ThrowsException<DivideByZeroException>(() => Program.Divide("5", "0"));
 	}
 
 	// Power method tests
